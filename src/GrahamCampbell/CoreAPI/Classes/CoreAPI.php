65aa485e6af0b07c841244679f0cab2bb390d441
Graham Campbell <graham@mineuk.com>
2013-08-16 11:21:12 +0100

    More new functions

--- src/GrahamCampbell/CoreAPI/Classes/CoreAPI.php
+++ src/GrahamCampbell/CoreAPI/Classes/CoreAPI.php
@@ -5,59 +5,90 @@ use Guzzle\Plugin\Oauth\OauthPlugin;
 
 class CoreAPI {
 
-    protected $url;
+    protected $baseurl;
     protected $config;
     protected $oauth;
+    protected $userAgent;
     protected $client;
 
-    protected function makeNewClient() {
-        $this->client = new Client($this->url, $this->config);
-
-        if ($this->oauth) {
-            $oauth = new OauthPlugin($oauth);
-            $this->client->addSubscriber($oauth);
-        }
-    }
-
-    public function setUp($url, $config = null, $oauth = null) {
-        $this->url = $url;
-        $this->config = $config;
+    public function setUp($baseurl, $config = array(), $oauth = null, $userAgent = null) {
+        $this->baseurl = $baseurl;
+        $this->config = new Collection($config);
         $this->oauth = $oauth;
+        $this->userAgent = $userAgent;
 
         $this->makeNewClient();
     }
 
+    protected function makeNewClient() {
+        $this->client = new Client($this->baseurl, $this->config);
+
+        if ($this->oauth) {
+            $this->setOauth($this->oauth);
+        }
+
+        if ($this->userAgent) {
+            $this->setUserAgent($this->userAgent);
+        }
+    }
+
     public function reset() {
-        if ($this->url) {
+        if ($this->baseurl) {
             $this->makeNewClient();
         }
     }
 
-    public function getURL() {
-        return $this->url;
+
+    public function getBaseUrl($expand = true) {
+        return $this->client->getBaseUrl($expand);
     }
 
-    public function setURL($value) {
-        $this->url = $value;
-        $this->makeNewClient();
+    public function setBaseUrl($baseurl) {
+        $this->baseurl = $baseurl;
+        return $this->client->setBaseUrl($this->baseurl);
     }
 
     public function getConfig() {
-        return $this->url;
+        return $this->client->getConfig();
     }
 
-    public function setConfig($value) {
-        $this->url = $value;
-        $this->makeNewClient();
+    public function setConfig($config) {
+        $this->config = new Collection($config);
+        return $this->client->setConfig($this->config);
     }
 
     public function getOauth() {
-        return $this->url;
+        return $this->oauth;
     }
 
-    public function setOauth($value) {
-        $this->url = $value;
-        $this->makeNewClient();
+    public function setOauth($oauth) {
+        $this->oauth = $oauth;
+        $oauth = new OauthPlugin($oauth);
+        $this->client->addSubscriber($oauth);
+    }
+
+    public function getUserAgent() {
+        return $this->userAgent();
+    }
+
+    public function getDefaultUserAgent() {
+        return $this->client->getDefaultUserAgent();
+    }
+
+    public function setUserAgent($userAgent, $includeDefault = false) {
+        if ($includeDefault) {
+            $userAgent .= ' ' . $this->getDefaultUserAgent();
+        }
+        $this->userAgent = $userAgent;
+        return $this->client->setUserAgent($this->userAgent);
+    }
+
+    public function setDefaultOption($keyOrPath, $value) {
+        return $this->client->setDefaultOption($keyOrPath, $value);
+    }
+
+    public function getDefaultOption($keyOrPath) {
+        return $this->client->getDefaultOption($keyOrPath);
     }
 
     public function getClient() {
@@ -65,8 +96,26 @@ class CoreAPI {
     }
 
     public function setClient($client) {
-        $this->client = $client;
+        return $this->client = $client;
     }
+
+    public function setSslVerification($certificateAuthority = true, $verifyPeer = true, $verifyHost = 2) {
+        return $this->client->setSslVerification($certificateAuthority, $verifyPeer, $verifyHost);
+    }
+
+
+    public function getAllEvents() {
+        return $this->client->getAllEvents();
+    }
+
+    public function createRequest($method = 'GET', $uri = null, $headers = null, $body = null, array $options = array()) {
+        return $this->client->createRequest($method, $uri, $headers, $body, $options);
+    }
+
+    public function send($requests) {
+        return $this->client->send($requests);
+    }
+
 
     public function get($uri = null, $headers = null, $options = array()) {
         return $this->client->get($uri, $headers, $options);
