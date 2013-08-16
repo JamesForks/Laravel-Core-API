81a1e005872f22db16db9b1f95955794c7b0d4e2
Graham Campbell <graham@mineuk.com>
2013-08-16 09:45:56 +0100

    More fixes and additions

--- src/GrahamCampbell/CoreAPI/Classes/CoreAPI.php
+++ src/GrahamCampbell/CoreAPI/Classes/CoreAPI.php
@@ -37,12 +37,20 @@ class CoreAPI {
         return $this->client;
     }
 
+    public function get($uri = null, $headers = null, $options = array()) {
+        return $this->client->get($uri, $headers, $options);
+    }
+
     public function sendGet($uri = null, $headers = null, $options = array()) {
         return $this->client->get($uri, $headers, $options)->send();
     }
 
     public function goGet($uri = null, $headers = null, $options = array()) {
         return $this->client->get($uri, $headers, $options)->send()->getBody();
+    }
+
+    public function head($uri = null, $headers = null, array $options = array()) {
+        return $this->client->head($uri, $headers, $options);
     }
 
     public function sendHead($uri = null, $headers = null, array $options = array()) {
@@ -53,12 +61,20 @@ class CoreAPI {
         return $this->client->head($uri, $headers, $options)->send()->getBody();
     }
 
+    public function delete($uri = null, $headers = null, $body = null, array $options = array()) {
+        return $this->client->delete($uri, $headers, $body, $options);
+    }
+
     public function sendDelete($uri = null, $headers = null, $body = null, array $options = array()) {
         return $this->client->delete($uri, $headers, $body, $options)->send();
     }
 
     public function goDelete($uri = null, $headers = null, $body = null, array $options = array()) {
         return $this->client->delete($uri, $headers, $body, $options)->send()->getBody();
+    }
+
+    public function put($uri = null, $headers = null, $body = null, array $options = array()) {
+        return $this->client->put($uri, $headers, $body, $options);
     }
 
     public function sendPut($uri = null, $headers = null, $body = null, array $options = array()) {
@@ -69,12 +85,20 @@ class CoreAPI {
         return $this->client->put($uri, $headers, $body, $options)->send()->getBody();
     }
 
+    public function patch($uri = null, $headers = null, $body = null, array $options = array()) {
+        return $this->client->patch($uri, $headers, $body, $options);
+    }
+
     public function sendPatch($uri = null, $headers = null, $body = null, array $options = array()) {
         return $this->client->patch($uri, $headers, $body, $options)->send();
     }
 
     public function goPatch($uri = null, $headers = null, $body = null, array $options = array()) {
         return $this->client->patch($uri, $headers, $body, $options)->send()->getBody();
+    }
+
+    public function post($uri = null, $headers = null, $body = null, array $options = array()) {
+        return $this->client->post($uri, $headers, $body, $options);
     }
 
     public function sendPost($uri = null, $headers = null, $body = null, array $options = array()) {
@@ -85,18 +109,15 @@ class CoreAPI {
         return $this->client->post($uri, $headers, $body, $options)->send()->getBody();
     }
 
+    public function options($uri = null, array $options = array()) {
+        return $this->client->options($uri, $options);
+    }
+
     public function sendOptions($uri = null, array $options = array()) {
         return $this->client->options($uri, $options)->send();
     }
 
     public function goOptions($uri = null, array $options = array()) {
         return $this->client->options($uri, $options)->send()->getBody();
-    }
-
-    // i hope this is correct...
-    private function __call($method, $args) {
-        if (in_array($method, self::$methods)) {
-            return call_user_func_array('$this->client->'.$method, $args);
-        }
     }
 }
