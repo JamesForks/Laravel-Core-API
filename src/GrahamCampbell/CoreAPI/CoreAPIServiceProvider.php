e4078e483c338be4f932f0c82b0427aa525969e0
Graham Campbell <graham@mineuk.com>
2013-08-17 14:48:18 +0100

    Updated the provders

--- src/GrahamCampbell/CoreAPI/CoreAPIServiceProvider.php
+++ src/GrahamCampbell/CoreAPI/CoreAPIServiceProvider.php
@@ -37,6 +37,6 @@ class CoreAPIServiceProvider extends ServiceProvider {
      * @return array
      */
     public function provides() {
-        return array();
+        return array('coreapi');
     }
 }
