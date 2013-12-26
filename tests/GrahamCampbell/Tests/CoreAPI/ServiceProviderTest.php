8578ed7266f770088bf57c5ae7f8d6258599f928
Graham Campbell <graham@mineuk.com>
2013-12-26 21:21:17 +0000

    Test refactor

--- tests/GrahamCampbell/Tests/CoreAPI/ServiceProviderTest.php
+++ tests/GrahamCampbell/Tests/CoreAPI/ServiceProviderTest.php
@@ -27,17 +27,7 @@ use GrahamCampbell\TestBench\Traits\ServiceProviderTestCaseTrait;
  * @license    https://github.com/GrahamCampbell/Laravel-Core-API/blob/develop/LICENSE.md
  * @link       https://github.com/GrahamCampbell/Laravel-Core-API
  */
-abstract class ServiceProviderTest extends AbstractTestCase
+class ServiceProviderTest extends AbstractTestCase
 {
     use ServiceProviderTestCaseTrait;
-
-    /**
-     * Get the service provider class.
-     *
-     * @return string
-     */
-    protected function getServiceProviderClass()
-    {
-        return 'GrahamCampbell\CoreAPI\CoreAPIServiceProvider';
-    }
 }
